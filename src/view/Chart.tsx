f9f74dad4a37d99e0beb5078ebd0ec93084bb6c8
miao <miao@163.com>
2021-09-10 23:02:11 +0800

    添加Chart组件

---
+++ src/view/Chart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function Chart() {
+  return (
+    <Layout>
+      <div>chart</div>
+    </Layout>
+  );
+}
+
+export default Chart;
